8ea98190f571b34bdb79e6ba9d5751f6ec524e70
Mani Kumar <manikumar@ManiBhushan.local>
2019-12-15 13:36:29 +0530

    fixed

--- viec/index/views.py
+++ viec/index/views.py
@@ -1,3 +1,11 @@
-from django.shortcuts import render
+from rest_framework import viewsets, status, permissions
+from rest_framework.renderers import TemplateHTMLRenderer, JSONRenderer
+from rest_framework.response import Response
 
 
+class IndexViewSet(viewsets.ViewSet):
+    permission_classes = [permissions.AllowAny]
+    renderer_classes = (JSONRenderer,TemplateHTMLRenderer)
+
+    def get(self, request, *args, **kwargs):
+        return Response(data={}, status=status.HTTP_200_OK, template_name='index/login.html')
--- viec/viec/settings.py
+++ viec/viec/settings.py
@@ -59,7 +59,7 @@ ROOT_URLCONF = 'viec.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': [os.path.join(BASE_DIR, 'index/templates')],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
@@ -123,3 +123,4 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/2.2/howto/static-files/
 
 STATIC_URL = '/static/'
+
