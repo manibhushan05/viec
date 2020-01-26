3916141f4782afb5682996d7e8260fee41ab84d1
Mani Kumar <manikumar@ManiBhushan.local>
2020-01-26 12:06:54 +0530

    models

--- viec/employee/models.py
+++ viec/employee/models.py
@@ -1,8 +1,6 @@
 from django.contrib.auth.models import User
 from django.db import models
 
-from utils.models import City
-
 
 class Profile(models.Model):
     user = models.OneToOneField(User, on_delete=models.CASCADE)
@@ -11,7 +9,7 @@ class Profile(models.Model):
     alternate_phone = models.CharField(max_length=30, blank=True, null=True)
     email = models.EmailField(max_length=100, blank=True, null=True)
     address = models.CharField(max_length=300, blank=True, null=True)
-    city = models.ForeignKey(City, blank=True, null=True, on_delete=models.CASCADE)
+    city = models.CharField(max_length=100, blank=True, null=True)
     pin = models.CharField(max_length=6, blank=True, null=True)
     designation = models.CharField(max_length=100, blank=True, null=True)
     remarks = models.TextField(blank=True, null=True)
@@ -34,8 +32,7 @@ class Employee(models.Model):
     email = models.EmailField(max_length=100, null=True)
     code = models.CharField(max_length=7)
 
-    created_by = models.ForeignKey(User, on_delete=models.CASCADE, related_name='employee_created_by')
-    created_at = models.DateTimeField(auto_now_add=True)
-    updated_at = models.DateTimeField(auto_now=True)
+    created_on = models.DateTimeField(auto_now_add=True)
+    updated_on = models.DateTimeField(auto_now=True)
     deleted = models.BooleanField(default=False)
-    deleted_by = models.ForeignKey(User, null=True, on_delete=models.CASCADE, related_name='employee_deleted_by')
+    deleted_on = models.DateTimeField(null=True, blank=True)
--- viec/utils/serializers.py
+++ viec/utils/serializers.py
@@ -2,7 +2,7 @@ from django.contrib.auth.models import User
 from rest_framework import serializers
 from rest_framework.validators import UniqueTogetherValidator, UniqueValidator
 
-from utils.models import City, State
+from utils.models import State
 
 
 class StateSerializer(serializers.Serializer):
@@ -26,38 +26,38 @@ class StateSerializer(serializers.Serializer):
         return State.objects.get(id=instance.id)
 
 
-class CitySerializer(serializers.Serializer):
-    id = serializers.IntegerField(label='ID', read_only=True)
-    name = serializers.CharField(max_length=200)
-    code = serializers.CharField(max_length=3, min_length=3, validators=[UniqueValidator(queryset=City.objects.all())])
-    pin = serializers.CharField(allow_null=True, max_length=6, min_length=6, required=False)
-    latitude = serializers.DecimalField(allow_null=True, decimal_places=12, max_digits=18, required=False)
-    longitude = serializers.DecimalField(allow_null=True, decimal_places=12, max_digits=18, required=False)
-    created_on = serializers.DateTimeField(read_only=True)
-    updated_on = serializers.DateTimeField(read_only=True)
-    deleted = serializers.BooleanField(required=False)
-    deleted_on = serializers.DateTimeField(allow_null=True, required=False)
-
-    created_by = serializers.SlugRelatedField(queryset=User.objects.all(), required=False, slug_field="username")
-    changed_by = serializers.SlugRelatedField(queryset=User.objects.all(), slug_field="username")
-    state = serializers.PrimaryKeyRelatedField(queryset=State.objects.all())
-
-    class Meta:
-        validators = [
-            UniqueTogetherValidator(
-                queryset=City.objects.all(),
-                fields=('name', 'state')
-            )
-        ]
-
-    def to_representation(self, instance):
-        self.fields["state"] = StateSerializer(read_only=True)
-        return super().to_representation(instance=instance)
-
-    def create(self, validated_data):
-        instance = City.objects.create(**validated_data)
-        return instance
-
-    def update(self, instance, validated_data):
-        City.objects.filter(id=instance.id).update(**validated_data)
-        return City.objects.get(id=instance.id)
+# class CitySerializer(serializers.Serializer):
+#     id = serializers.IntegerField(label='ID', read_only=True)
+#     name = serializers.CharField(max_length=200)
+#     code = serializers.CharField(max_length=3, min_length=3, validators=[UniqueValidator(queryset=City.objects.all())])
+#     pin = serializers.CharField(allow_null=True, max_length=6, min_length=6, required=False)
+#     latitude = serializers.DecimalField(allow_null=True, decimal_places=12, max_digits=18, required=False)
+#     longitude = serializers.DecimalField(allow_null=True, decimal_places=12, max_digits=18, required=False)
+#     created_on = serializers.DateTimeField(read_only=True)
+#     updated_on = serializers.DateTimeField(read_only=True)
+#     deleted = serializers.BooleanField(required=False)
+#     deleted_on = serializers.DateTimeField(allow_null=True, required=False)
+#
+#     created_by = serializers.SlugRelatedField(queryset=User.objects.all(), required=False, slug_field="username")
+#     changed_by = serializers.SlugRelatedField(queryset=User.objects.all(), slug_field="username")
+#     state = serializers.PrimaryKeyRelatedField(queryset=State.objects.all())
+#
+#     class Meta:
+#         validators = [
+#             UniqueTogetherValidator(
+#                 queryset=City.objects.all(),
+#                 fields=('name', 'state')
+#             )
+#         ]
+#
+#     def to_representation(self, instance):
+#         self.fields["state"] = StateSerializer(read_only=True)
+#         return super().to_representation(instance=instance)
+#
+#     def create(self, validated_data):
+#         instance = City.objects.create(**validated_data)
+#         return instance
+#
+#     def update(self, instance, validated_data):
+#         City.objects.filter(id=instance.id).update(**validated_data)
+#         return City.objects.get(id=instance.id)
